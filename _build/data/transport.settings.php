62eb0a25b6e8fa3e7d6099e2198e6753e9360fc8
Володя Володин <vovanblya@mail.ru>
2014-12-20 13:44:49 +0400

    initial [build]

--- _build/data/transport.settings.php
+++ _build/data/transport.settings.php
@@ -2,7 +2,33 @@
 
 $settings = array();
 
-$tmp = array(/*
+$tmp = array(
+
+
+
+
+	//временные
+
+			'assets_path' => array(
+				'xtype' => 'textfield',
+				'value' => '{base_path}currencyrate/assets/components/currencyrate/',
+				'area' => 'currencyrate_temp',
+			),
+			'assets_url' => array(
+				'xtype' => 'textfield',
+				'value' => '/currencyrate/assets/components/currencyrate/',
+				'area' => 'currencyrate_temp',
+			),
+			'core_path' => array(
+				'xtype' => 'textfield',
+				'value' => '{base_path}currencyrate/core/components/currencyrate/',
+				'area' => 'currencyrate_temp',
+			),
+
+
+	//временные
+
+		/*
 	'some_setting' => array(
 		'xtype' => 'combo-boolean',
 		'value' => true,
