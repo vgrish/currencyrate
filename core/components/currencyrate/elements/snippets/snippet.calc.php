9bff922ea60ab5a3e7ef6303f015198254b2b985
Володя Володин <vovanblya@mail.ru>
2014-12-21 14:17:55 +0400

    + date

--- core/components/currencyrate/elements/snippets/snippet.calc.php
+++ core/components/currencyrate/elements/snippets/snippet.calc.php
@@ -5,10 +5,13 @@ if (!$currencyrate = $modx->getService('currencyrate', 'currencyrate', $modx->ge
 	return 'Could not load currencyrate class!';
 }
 if(empty($input)) {return '';}
+$list = $currencyrate->getList();
 if(!empty($multiplier)) {
+	$multiplier = $list[$multiplier];
 	$output = $currencyrate->formatPrice(($input * $multiplier), $format, $noZeros);
 }
 if(!empty($divider)) {
+	$divider = !empty($list[$divider]) ? $list[$divider] : 1;
 	$output = $currencyrate->formatPrice(($input / $divider), $format, $noZeros);
 }
 
