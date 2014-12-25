ccd8c2b477dfdb84348e8a6ca3f5f67d2b7de2b9
Володя Володин <vovanblya@mail.ru>
2014-12-25 17:04:33 +0400

    + date

--- assets/components/currencyrate/connector.php
+++ assets/components/currencyrate/connector.php
@@ -10,12 +10,7 @@ if (file_exists(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php
 } else {
 	require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/config.core.php';
 }
-
-/** @noinspection PhpIncludeInspection */
-//require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
-/** @noinspection PhpIncludeInspection */
 require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
-/** @noinspection PhpIncludeInspection */
 require_once MODX_CONNECTORS_PATH . 'index.php';
 /** @var currencyrate $currencyrate */
 $currencyrate = $modx->getService('currencyrate', 'currencyrate', $modx->getOption('currencyrate_core_path', null, $modx->getOption('core_path') . 'components/currencyrate/') . 'model/currencyrate/');
---
+++ assets/components/currencyrate/web-connector.php
@@ -0,0 +1,18 @@
+<?php
+// For debug
+ini_set('display_errors', 1);
+ini_set('error_reporting', -1);
+
+// Load MODX config
+if (file_exists(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php')) {
+	require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/config.core.php';
+} else {
+	require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/config.core.php';
+}
+require_once MODX_CORE_PATH . 'config/' . MODX_CONFIG_KEY . '.inc.php';
+require_once MODX_CONNECTORS_PATH . 'index.php';
+
+$Referral = $modx->getService('referral', 'Referral', $modx->getOption('referral_core_path', null, $modx->getOption('core_path') . 'components/referral/') . 'model/referral/');
+$modx->lexicon->load('referral:default');
+
+$currencyrate->rateIntoDb();
